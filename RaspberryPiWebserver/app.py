d7237fc2b2746f8f92e04424bf0a8468b1fa5673
Anatol-Gogoj <anatol.gogoj@uconn.edu>
2025-07-08 17:19:01 -0400

    Merged mjpeg generator with app.py

--- RaspberryPiWebserver/app.py
+++ RaspberryPiWebserver/app.py
@@ -1,10 +1,44 @@
 # app.py
 
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template, request, Response
+import subprocess
+
 
 app = Flask(__name__)
 ARDUINO_IP = '192.168.0.227'
+
+
+def generate_mjpeg():
+    # Start libcamera-vid in MJPEG stream mode
+    cmd = [
+        "libcamera-vid",
+        "-t", "0",              # unlimited time
+        "--inline",             # required for MJPEG
+        "--codec", "mjpeg",
+        "--width", "640",
+        "--height", "480",
+        "-o", "-"               # output to stdout
+    ]
+
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=0)
+
+    try:
+        while True:
+            # Read the JPEG frame by scanning for JPEG markers
+            data = process.stdout.read(4096)
+            if data:
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' +
+                       data + b'\r\n')
+    except GeneratorExit:
+        process.terminate()
+        process.wait()
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_mjpeg(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @app.route('/')
 def index():
